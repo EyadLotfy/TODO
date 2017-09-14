1e89ccc442fa9fcbffdbd00eb83a1f236f6e72c2
EyadLotfy <yodda_hunter@hotmail.com>
2017-09-14 16:26:12 -0700

    savenewtask II
    Still save version of saveNewTask but has been edited to match the standred Python code criteria.

--- TODO/saveNewTask.py
+++ TODO/saveNewTask.py
@@ -1,26 +1,27 @@
+'''this module creates & saves new tasks to "task.txt" file.'''
 def save_task(task_context):
+    '''saves a new task into "task.txt" file.'''
     try:
-        file = open('tasks.txt','a')
-        file.write('\n-%s'%task_context)
+        file_handler = open('tasks.txt', 'a')
+        file_handler.write('\n-%s'%task_context)
         return 1
     except:
         return 0
-
 def add_new_task():
+    '''adds new task and process it to be saved in "task.txt" file.'''
     task_context = raw_input('New task>>>')
     if save_task(task_context) == 1:
         return 1
     else:
         return 0
-    
 def start():
+    '''starting function.'''
     if add_new_task() == 1:
         return 1
     else:
         return 0
-
 while True:
     if start() == 1:
-        print('New task was saved!')
+        print 'New task was saved!'
     else:
-        print('Some failure occured!')
+        print 'Some failure occured!'
